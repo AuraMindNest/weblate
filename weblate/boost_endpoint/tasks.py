5da2b1875f535eedc6979b598eeecbf86211f7dc
AuraMindNest <william.connects@outlook.com>
2026-05-07 11:33:51 -0600

    Restore some parts of boost_endpoint.

--- weblate/boost_endpoint/tasks.py
+++ weblate/boost_endpoint/tasks.py
@@ -39,5 +39,7 @@ def boost_add_or_update_task(
             version=version,
             extensions=extensions,
         )
-        results[lang_code] = service.process_all(submodules, user=user, request=request)
+        results[lang_code] = service.process_all(
+            submodules, user=user, request=request
+        )
     return results
